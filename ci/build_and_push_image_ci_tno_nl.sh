1c82dca7d21b8259cfe08329e7174258dcd2521e
leo.vanschooten-tue <l.g.t.v.schooten@tue.nl>
2023-12-21 13:23:43 +0100

    update version
    # Conflicts:
    #	k8s/so-rest-deployment.yaml

--- ci/build_and_push_image_ci_tno_nl.sh
+++ ci/build_and_push_image_ci_tno_nl.sh
@@ -2,7 +2,7 @@
 
 # login first: docker login -u <ci.tno.nl email address> ci.tno.nl
 
-VERSION=0.0.2
+VERSION=0.0.3
 REPOSITORY="ci.tno.nl/dots/simulation-orchestrator"
 
 docker build -t ${REPOSITORY}:${VERSION} ./..
